20e168844a4591a087d559395799005bf421f7f8
AksShri2004 <shrivastavaakshat2004@gmail.com>
2023-12-14 03:07:02 +0530

    initial

---
+++ stock-news-extrahard-start/main.py
@@ -0,0 +1,37 @@
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+import requests
+from datetime import date
+
+print(date.today())
+## STEP 1: Use https://www.alphavantage.co
+# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+url="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&interval=60min&apikey=SZ0FOZU6GGMIB6LC"
+r = requests.get(url)
+data = r.json()
+
+keys=list(data['Time Series (Daily)'].keys())
+price_yes=data['Time Series (Daily)'][keys[0]]["4. close"]
+price_bef_yes=data['Time Series (Daily)'][keys[1]]["4. close"]
+if price_yes-price_bef_yes>= 0.05*price_bef_yes or price_bef_yes-price_yes >= 0.05*price_bef_yes:
+    print("Get news")
+
+
+## STEP 2: Use https://newsapi.org
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+
+## STEP 3: Use https://www.twilio.com
+# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+
+
+#Optional: Format the SMS message like this: 
+"""
+TSLA: 🔺2%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+or
+"TSLA: 🔻5%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+"""
+
